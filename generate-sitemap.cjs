958edd7e0e58b5d22a2e355def12793b61f9863a
Radu Nicolau <radu.alex.nicolau@gmail.com>
2025-11-18 09:01:51 +0000

    sitemap

--- generate-sitemap.cjs
+++ generate-sitemap.cjs
@@ -79,8 +79,7 @@ function createSitemap(entries) {
     .join('');
 
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${xmlUrls}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${xmlUrls}
 </urlset>`;
 }
 
